a091bad457d1689ae4bc3a956cc681877cfe9e3b
blargg <anuibis@sbcglobal.net>
2016-09-04 10:08:46 -0500

    Improved test documentation

--- src-test/DeltaCRDTSpec.hs
+++ src-test/DeltaCRDTSpec.hs
@@ -14,18 +14,9 @@ import Test.Tasty.QuickCheck
 
 -- Applying a patch always moves up in the order.
 -- Implies that there was some value y such that
--- `apply delta x == x /\ y`
+-- `apply delta x == x \/ y`
 advances :: (PartialOrd a, DCRDT a) => a -> Delta a -> Bool
 advances x delta = x `leq` apply delta x
-
-setIsDCRDT :: [TestTree]
-setIsDCRDT =
-    [ testProperty "Advances" (advances :: Set Int -> Delta (Set Int) -> Bool)
-    , testProperty "Apply Idempotent" (applyIsIdempotent :: Set Int -> Delta (Set Int) -> Bool)
-    , testProperty "Deltas Merge" (deltaMerge :: Set Int -> Delta (Set Int) -> Delta (Set Int) -> Bool)
-    , testProperty "Merge Commutes" (mergeCommutes :: Set Int -> Delta (Set Int) -> Delta (Set Int) -> Bool)
-    ]
-
 
 applyIsIdempotent :: (Eq a, DCRDT a) => a -> Delta a -> Bool
 applyIsIdempotent x delta = apply delta (apply delta x) == apply delta x
@@ -37,3 +28,11 @@ deltaMerge x d1 d2 = apply (merge d1 d2) x == apply d2 (apply d1 x)
 -- Deltas can be merged in any order and produce the same result
 mergeCommutes :: (Eq a, DCRDT a) => a -> Delta a -> Delta a -> Bool
 mergeCommutes x d1 d2 = apply (merge d1 d2) x == apply (merge d2 d1) x
+
+setIsDCRDT :: [TestTree]
+setIsDCRDT =
+    [ testProperty "Advances" (advances :: Set Int -> Delta (Set Int) -> Bool)
+    , testProperty "Apply Idempotent" (applyIsIdempotent :: Set Int -> Delta (Set Int) -> Bool)
+    , testProperty "Deltas Merge" (deltaMerge :: Set Int -> Delta (Set Int) -> Delta (Set Int) -> Bool)
+    , testProperty "Merge Commutes" (mergeCommutes :: Set Int -> Delta (Set Int) -> Delta (Set Int) -> Bool)
+    ]
