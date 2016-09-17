ff6fcbe14f95a8980bb0524bac2ec6d6f112a8fb
blargg <anuibis@sbcglobal.net>
2016-09-17 18:45:39 -0500

    Remove merge from DCRDT

--- src-test/DeltaCRDTSpec.hs
+++ src-test/DeltaCRDTSpec.hs
@@ -5,34 +5,18 @@ module DeltaCRDTSpec where
 
 import DeltaCRDT
 import Data.Set
-import Algebra.PartialOrd
 
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
-
-
--- Applying a patch always moves up in the order.
--- Implies that there was some value y such that
--- `apply delta x == x \/ y`
-advances :: (PartialOrd a, DCRDT a) => a -> Delta a -> Bool
-advances x delta = x `leq` apply delta x
-
 applyIsIdempotent :: (Eq a, DCRDT a) => a -> Delta a -> Bool
 applyIsIdempotent x delta = apply delta (apply delta x) == apply delta x
 
--- applying merged patches is the same as applying them seperately
-deltaMerge :: (Eq a, DCRDT a) => a -> Delta a -> Delta a -> Bool
-deltaMerge x d1 d2 = apply (merge d1 d2) x == apply d2 (apply d1 x)
-
--- Deltas can be merged in any order and produce the same result
-mergeCommutes :: (Eq a, DCRDT a) => a -> Delta a -> Delta a -> Bool
-mergeCommutes x d1 d2 = apply (merge d1 d2) x == apply (merge d2 d1) x
+applyCommutes :: (Eq a, DCRDT a) => a -> Delta a -> Delta a -> Bool
+applyCommutes a d1 d2 = apply d2 (apply d1 a) == apply d1 (apply d2 a)
 
 setIsDCRDT :: [TestTree]
 setIsDCRDT =
-    [ testProperty "Advances" (advances :: Set Int -> Delta (Set Int) -> Bool)
-    , testProperty "Apply Idempotent" (applyIsIdempotent :: Set Int -> Delta (Set Int) -> Bool)
-    , testProperty "Deltas Merge" (deltaMerge :: Set Int -> Delta (Set Int) -> Delta (Set Int) -> Bool)
-    , testProperty "Merge Commutes" (mergeCommutes :: Set Int -> Delta (Set Int) -> Delta (Set Int) -> Bool)
+    [ testProperty "Apply Idempotent" (applyIsIdempotent :: Set Int -> Delta (Set Int) -> Bool)
+    , testProperty "Apply Commutes" (applyCommutes :: Set Int -> Delta (Set Int) -> Delta (Set Int) -> Bool)
     ]
--- src/DeltaCRDT.hs
+++ src/DeltaCRDT.hs
@@ -15,20 +15,17 @@ import Test.QuickCheck.Arbitrary
 class DCRDT a where
     data Delta a :: *
     apply :: Delta a -> a -> a
-    merge :: Delta a -> Delta a -> Delta a
 
 
 instance (Ord a) => DCRDT (Ordered a) where
     data Delta (Ordered a) = DeltaOrdered a deriving (Show, Generic)
     apply (DeltaOrdered d) a = Ordered d \/ a
-    merge (DeltaOrdered d) (DeltaOrdered d2) = DeltaOrdered . getOrdered $ (Ordered d) \/ (Ordered d2)
 
 instance (Serialize a) => Serialize (Delta (Ordered a))
 
 instance (Ord a) => DCRDT (S.Set a) where
     data Delta (S.Set a) = DeltaSet (S.Set a) deriving (Show, Eq, Generic)
     apply (DeltaSet d) x = d `S.union` x
-    merge (DeltaSet d) (DeltaSet d2) = DeltaSet (S.union d d2)
 
 instance (Ord a, Arbitrary a) => Arbitrary (Delta (S.Set a)) where
     arbitrary = DeltaSet . S.fromList <$> arbitrary
@@ -37,4 +34,3 @@ instance (Ord a, Arbitrary a) => Arbitrary (Delta (S.Set a)) where
 instance (Ord k, DCRDT a) => DCRDT (TotalMap k a) where
     data Delta (TotalMap k a) = DeltaTotalMap (TotalMap k (Delta a))
     apply (DeltaTotalMap d) x = apply <$> d <*> x
-    merge (DeltaTotalMap d1) (DeltaTotalMap d2) = DeltaTotalMap $ merge <$> d1 <*> d2
