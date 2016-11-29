8d0b3810bce2cce8d0a799dded8713f38f2080ca
blargg <anuibis@sbcglobal.net>
2016-11-28 21:52:39 -0600

    Options Applicative command line interface

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,25 +1,39 @@
 import AntiEntropy
-import DeltaCRDT
 import Algebra.Lattice.Ordered
+import Control.Monad (when)
+import Options.Applicative
+import Network.Socket (PortNumber(..))
 
-import System.Environment (getArgs)
 import System.Log.Logger
 
-localIP :: String
-localIP = "127.0.0.1"
+data NodeSettings = NodeSettings { port :: PortNumber
+                                 , debug :: Bool
+                                 , neighborURLs :: [String]
+                                 }
 
-serverPort :: String
-serverPort = "3333"
+nodeParser :: Parser NodeSettings
+nodeParser = NodeSettings
+    <$> option auto
+        ( long "port"
+        <> metavar "INT"
+        <> value 4400
+        <> help "The port that the program runs on")
+    <*> switch
+        ( long "debug"
+        <> short 'd'
+        <> help "When set, prints debug information")
+    <*> many (strArgument
+        ( metavar "ADDRESS"
+        <> help "Neighbor Addresses"))
+
+programSettings :: ParserInfo NodeSettings
+programSettings = info (helper <*> nodeParser)
+    (fullDesc
+    <> progDesc "Runs a server node that replicates data across all nodes in the system"
+    <> header "Conflict Free Replicated Data Types (CRDT)")
 
 main :: IO ()
 main = do
-    updateGlobalLogger "" (setLevel DEBUG)
-    args <- getArgs
-    case args of
-         "server":port:ns -> runNode (Ordered (0::Int)) (read port) ns
-         "client":param:_ -> runClient (read param)
-         _ -> putStrLn "run as server or client <integer>"
-
-
-runClient :: Int -> IO ()
-runClient value = simpleSend 9876 localIP serverPort (Payload (DeltaOrdered value) 1)
+    settings <- execParser programSettings
+    when (debug settings) $ updateGlobalLogger "" (setLevel DEBUG)
+    runNode (Ordered (0::Int)) (port settings) (neighborURLs settings)
