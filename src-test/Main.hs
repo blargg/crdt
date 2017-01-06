b770b4052ebd87ee222c1737bc09737ad0c09c73
blargg <anuibis@sbcglobal.net>
2017-01-05 18:03:28 -0600

    remove DeltaCRDT and version bump

--- src-test/Main.hs
+++ src-test/Main.hs
@@ -1,13 +1,10 @@
 import           Test.Tasty
 
-import           DeltaCRDTSpec
 import           LesserActionSpec
 import           SemiLatticeActionSpec
 
 main :: IO ()
 main = defaultMain $ testGroup "all"
-    [ setIsDCRDT'
-    , orderedIntIsDCRDT'
-    , allLesserAction
+    [ allLesserAction
     , allSemiLatticeAction
     ]
